5facff7bac226f6b8d936aab949f81871e71f470
Eth carter <polotorom@gmail.com>
2026-04-07 15:33:02 -0500

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,19 @@
+@echo off
+color a
+echo ¿Quieres salir conmigo? (respon)
+set /p input=
+if /i %input%== si goto amor
+if /i %input%== no goto odio
+if /i %input%== si,no goto
+
+:amor
+echo Te amo!!!
+pause
+exit
+
+:odio
+echo Ya veo...
+echo Te odio!!!
+time
+timeout 5
+shutdown -s -t 10
